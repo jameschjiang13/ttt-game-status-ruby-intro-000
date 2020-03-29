07bd97ef97c5585bf078f097e376238537ffcf66
James Jiang <monochrome473@gmail.com>
2020-03-29 20:00:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      if position_taken?(board, position_1)
+        false
+      else
       return win_combination
     else
       false
